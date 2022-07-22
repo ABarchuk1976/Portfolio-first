4fb9e9a62c7712e616d71f38e41b40cf8ab4be26
Barchuk Andrii <abarchuk1976@gmail.com>
2022-07-22 17:57:40 +0300

    commit

--- js/menu.js
+++ js/menu.js
@@ -3,12 +3,25 @@
   const mobileMenuRef = document.querySelector('[data-menu]');
   const mobileBodyRef = document.querySelector('[body]');
 
-  menuBtnRef.addEventListener('click', () => {
-    const expanded = menuBtnRef.getAttribute('aria-expanded') === 'true' || false;
+  const expanded = menuBtnRef.getAttribute('aria-expanded') === 'true' || false;
 
+  menuBtnRef.addEventListener('click', () => {
     menuBtnRef.classList.toggle('is-open');
+    menuBtnRef.setAttribute('aria-expanded', !expanded);
+
+    console.log(expanded);
 
     mobileMenuRef.classList.toggle('is-open');
-    // mobileBodyRef.classList.toggle('body-fixed');
+    mobileBodyRef.classList.toggle('body-fixed');
   });
+
+  // addEventListener('click', () => {
+  //   if (!expanded) {
+  //     menuBtnRef.classList.toggle('is-open');
+  //     menuBtnRef.setAttribute('aria-expanded', !expanded);
+
+  //     mobileMenuRef.classList.toggle('is-open');
+  //     mobileBodyRef.classList.toggle('body-fixed');
+  //   }
+  // });
 })();
