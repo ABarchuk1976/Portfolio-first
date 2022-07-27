ef433216b5ee5feec51cfbd1e12305291c7b418d
Barchuk Andrii <abarchuk1976@gmail.com>
2022-07-27 16:31:45 +0300

    Fixed classes

--- js/menu-arrow.js
+++ js/menu-arrow.js
@@ -1,9 +1,15 @@
-const trueCallback = function (observer) {
-  console.log('сработало');
-  target.classList.toggle('is-visible');
+const trueCallback = function (entries, observer) {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      target.classList.add('is-visible');
+    } else {
+      target.classList.remove('is-visible');
+    } // intersecting: true or false
+  });
 };
 
 const options = {
+  root: null,
   rootMargin: '0px',
   threshold: 0,
 };
@@ -11,4 +17,5 @@ const options = {
 const observer = new IntersectionObserver(trueCallback, options);
 
 const target = document.querySelector('.header');
+
 observer.observe(target);
